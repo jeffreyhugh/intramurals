fa5302bbf86c7e51e1dda52d90bf832f4c5aafd5
Jeffrey Hugh <j@jh.ms>
2023-02-27 21:07:09 -0600

    feat: add my name

---
+++ pages/jeff.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import { Breadcrumbs } from '@/components/layout/Breadcrumbs';
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+export default function Page() {
+  return (
+    <Layout>
+      <Seo templateTitle='Jeff' />
+
+      <main className='flex flex-grow'>
+        <section className='flex flex-grow'>
+          <div className='layout min-h-c'>
+            <Breadcrumbs />
+            <h1 className='text-4xl font-bold'>Jeff</h1>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
