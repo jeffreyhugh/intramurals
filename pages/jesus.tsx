faf70ef6af82d83cd8b6c9832ffee0aaf73a6626
JABAROP <jesusabaro1@gmail.com>
2023-02-28 03:09:14 -0600

    feat: add my name

---
+++ pages/jesus.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import { Breadcrumbs } from '@/components/layout/Breadcrumbs';
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+export default function Page() {
+  return (
+    <Layout>
+      <Seo templateTitle='Jesus' />
+
+      <main className='flex flex-grow'>
+        <section className='flex flex-grow'>
+          <div className='layout min-h-c'>
+            <Breadcrumbs />
+            <h1 className='text-4x1 font-bold'>Jesus</h1>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
