8d36fb7db13ae6937d87a2bf4dbe18d3f5604eef
Jeffrey Hugh <j@jh.ms>
2023-03-01 11:36:02 -0600

    feat: add logo and update branding

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -5,9 +5,9 @@ export default function Footer() {
     <footer className='footer footer-center absolute bottom-0 border-t border-t-neutral/10 bg-dark-100 p-4 text-light-100'>
       <div>
         <p>
-          &copy; {new Date().getFullYear()} by{' '}
-          <Link className='link' href='https://jh.ms'>
-            Jeffrey Hugh
+          &copy; {new Date().getFullYear()}{' '}
+          <Link className='link' href='https://intramurals.net'>
+            Intramurals.net
           </Link>
         </p>
       </div>
