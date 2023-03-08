47b5908f05dd786ca5416008dbaa860c5d99dbd8
Jeffrey Hugh <j@jh.ms>
2023-03-07 20:16:54 -0600

    feat: add demo code for hooks

--- pages/jeff.tsx
+++ pages/jeff.tsx
@@ -1,10 +1,30 @@
 import * as React from 'react';
 
+import { useLocations } from '@/lib/hooks/useLocations';
+
 import { Breadcrumbs } from '@/components/layout/Breadcrumbs';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
+import Skeleton from '@/components/Skeleton';
 
 export default function Page() {
+  // const data = { a: 3, b: 5 };
+  // const { a, b } = data;
+
+  // console.log(a)  // 3
+
+  // const numbers = [1, 2, 3]
+  // console.log(numbers.map(number => number * 2))  // 2, 4, 6
+
+  const { data, isLoading, error } = useLocations();
+
+  if (error) {
+    //eslint-disable-next-line no-console
+    console.error(error);
+  }
+
+  // console.log(data);
+
   return (
     <Layout>
       <Seo templateTitle='Jeff' />
@@ -14,6 +34,17 @@ export default function Page() {
           <div className='layout min-h-c'>
             <Breadcrumbs />
             <h1 className='text-4xl font-bold'>Jeff</h1>
+            {isLoading ? (
+              <Skeleton className='h-8 w-full' />
+            ) : !data ? (
+              <>No data</>
+            ) : (
+              data.map((location) => (
+                <div key={location.id}>
+                  {location.friendly_name} is at {location.latlong}
+                </div>
+              ))
+            )}
           </div>
         </section>
       </main>
--- pages/jesus.tsx
+++ pages/jesus.tsx
@@ -1,10 +1,24 @@
 import * as React from 'react';
 
+import { useLocation } from '@/lib/hooks/useLocation';
+
 import { Breadcrumbs } from '@/components/layout/Breadcrumbs';
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
+import Skeleton from '@/components/Skeleton';
 
 export default function Page() {
+  const { data, isLoading, error } = useLocation(
+    'c9d074e9-0f5e-4a2e-bc3c-8fb128264d5a'
+  );
+
+  if (error) {
+    //eslint-disable-next-line no-console
+    console.error(error);
+  }
+
+  // console.log(data);
+
   return (
     <Layout>
       <Seo templateTitle='Jesus' />
@@ -14,6 +28,14 @@ export default function Page() {
           <div className='layout min-h-c'>
             <Breadcrumbs />
             <h1 className='text-4xl font-bold'>Jesus</h1>
+
+            {isLoading ? (
+              <Skeleton className='h-8 w-full' />
+            ) : !data ? (
+              <>No data</>
+            ) : (
+              <div>Friendly name: {data.friendly_name}</div>
+            )}
           </div>
         </section>
       </main>
