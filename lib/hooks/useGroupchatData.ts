3bc43ce0a5d55f87b0634e3fd3aa6ce692c52ae2
Jeffrey Hugh <j@jh.ms>
2023-04-12 19:40:05 -0500

    feat: add groupchat hooks

---
+++ lib/hooks/useGroupchatData.ts
@@ -0,0 +1,35 @@
+import {
+  SupabaseClient,
+  useSupabaseClient,
+} from '@supabase/auth-helpers-react';
+import useSWR from 'swr';
+
+import { Database } from '@/lib/types/database.types';
+
+/*
+ * Use this hook to get messages for the groupchat
+ */
+export const useGroupchatData = (groupchatID: string) => {
+  const client = useSupabaseClient<Database>();
+
+  return useSWR(`groupchatData-${groupchatID}`, () =>
+    groupchatDataFetcher(client, groupchatID)
+  );
+};
+
+export const groupchatDataFetcher = async (
+  client: SupabaseClient<Database, 'public'>,
+  groupchatID: string
+) => {
+  const { data, error } = await client
+    .from('groupchat_data_friendly')
+    .select('*')
+    .eq('groupchat_id', groupchatID)
+    .order('message_time', { ascending: false });
+
+  if (error) {
+    throw error;
+  }
+
+  return data || [];
+};
