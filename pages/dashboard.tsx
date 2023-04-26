981529dcc4b679f68e4ee9b6ef3153ba30107bbb
Jesus <jesusabaro1@gmail.com>
2023-04-26 00:16:51 -0500

    feat: added dashboard features

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,11 @@
+import { useSupabaseClient, useUser } from '@supabase/auth-helpers-react';
 import * as React from 'react';
+import { useState } from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
 
+import { useMetadata } from '@/lib/hooks/useMetadata';
 import { requireAuth } from '@/lib/requireAuth';
+import { Database } from '@/lib/types/database.types';
 
 import { Breadcrumbs } from '@/components/layout/Breadcrumbs';
 import Layout from '@/components/layout/Layout';
@@ -8,6 +13,29 @@ import Seo from '@/components/Seo';
 
 export default function Page() {
   // TODO what data do we need here?
+
+  const supabase = useSupabaseClient<Database>();
+  const user = useUser();
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
+  };
+
+  const data = useMetadata();
+  const wins = data?.data?.wins || 0;
+  const losses = data?.data?.loses || 0;
+  const total = wins + losses;
+  const winLossRatio =
+    data?.data?.wins && data?.data?.loses
+      ? (data.data.wins + data.data.loses) * 100
+      : 0;
+
+  const piedata = [
+    { title: 'Wins', value: data?.data?.wins || 1, color: '#009944' },
+    { title: 'Losses', value: data?.data?.loses || 1, color: '#EA3546' },
+  ];
 
   return (
     <Layout>
@@ -17,10 +45,83 @@ export default function Page() {
         <section className='flex flex-grow'>
           <div className='layout min-h-c'>
             <Breadcrumbs />
-            <h1 className='mt-6 text-4xl font-bold'>Dashboard</h1>
-            <div className='mt-6 w-full'>
-              <>TODO what data do we need here?</>
+            <h1 className='mt-6 text-4xl font-bold'> Dashboard </h1>
+            <h2 className='mt-6 text-4xl font-bold'>
+              {' '}
+              Welcome, {data?.data?.first_name}{' '}
+            </h2>
+            <div className='bg-dark mt-6 w-full flex-auto justify-between'>
+              <div className='m-5 flex h-28 w-28 justify-center rounded-full bg-primary'>
+                <div className='h-18 w-18 m-2 flex justify-center overflow-hidden  rounded-full bg-white'>
+                  <label htmlFor='image-upload'> Upload Image: </label>
+                  <input
+                    type='file'
+                    accept='image/*'
+                    id='image-upload'
+                    onChange={handleImageChange}
+                  />
+
+                  <img
+                    src='/images/stock_Image.png'
+                    alt='uploaded'
+                    className='ml-3 mt-1 mb-1 w-full'
+                  />
+
+                  <div className='m-2 flex h-56 w-56 rounded-full'>
+                    {' '}
+                    Insert Image Here...
+                  </div>
+                </div>
+              </div>
+              <div className='bg-dark mt-1 flex w-full border'>
+                <div className='bg-dark align-center mt-0 flex w-1/3 flex-col content-center justify-start border-2 text-center'>
+                  <h1 className='mt-3 text-center text-4xl font-bold'>
+                    {' '}
+                    Wins{' '}
+                  </h1>
+                  <div className='bg-dark mt-28 w-full text-4xl'>
+                    {' '}
+                    {data?.data?.wins ? data?.data?.wins : 0}{' '}
+                  </div>
+                </div>
+                <div className='bg-dark align-center mt-0 flex w-1/3 flex-col content-center justify-start border-2 text-center'>
+                  <h1 className='mt-3 text-center text-4xl font-bold'>
+                    {' '}
+                    Losses{' '}
+                  </h1>
+                  <div className='bg-dark mt-28 w-full text-4xl'>
+                    {' '}
+                    {data?.data?.loses ? data?.data?.loses : 0}{' '}
+                  </div>
+                </div>
+                <div className='bg-dark align-center mt-0 flex w-1/3 flex-col content-center justify-start border-2 text-center'>
+                  <h1 className='mt-3 text-center text-4xl font-bold'>
+                    {' '}
+                    Win %{' '}
+                  </h1>
+                  <div className='bg-dark mt-28 w-full text-4xl'>
+                    {' '}
+                    {winLossRatio.toFixed(2)}%
+                  </div>
+                </div>
+              </div>
+
+              <PieChart
+                className='w-50 mt-12 h-80 flex-auto'
+                data={piedata}
+                lineWidth={100}
+                paddingAngle={0}
+                label={({ dataEntry }) => dataEntry.value - 1}
+                labelStyle={{
+                  fontSize: '10px',
+                  fontFamily: 'sans-serif',
+                  fill: '#fff',
+                }}
+              />
             </div>
+
+            {/* <>TODO, profile image insert, wins, loss, w/l ratio, </>
+              <>div for profile, div for stats</> */}
           </div>
         </section>
       </main>
